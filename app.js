e022e497bdbfde54039c2590b0f342f2f122e8c3
Dongri Jin <dongrify@gmail.com>
2019-01-09 22:38:51 +0900

    Fix

--- app.js
+++ app.js
@@ -152,6 +152,6 @@ function ensureAuthenticated(req, res, next) {
   }
 }
 
-http.createServer(app).listen(app.get('port'), '127.0.0.1', function(){
+http.createServer(app).listen(app.get('port'), '0.0.0.0', function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
